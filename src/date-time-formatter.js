78bfbd40b457f92afa599d26ea5baf877740e5cf
Qiujuan Yuan <qiujuan@openresty.com>
2025-01-13 14:59:11 +0800

    feat: added format timestamp function.

--- src/date-time-formatter.js
+++ src/date-time-formatter.js
@@ -1,7 +1,8 @@
 /**
- * convert countdown number to
+ * @description convert countdown number
  * @param timestamp <Number> seconds
- * @param format <String> 'hh:mm:ss' || 'dd:hh:mm:ss', default 'hh:mm:ss'
+ * @param format<String> 'hh:mm:ss' || 'dd:hh:mm:ss', default 'hh:mm:ss'
+ * @returns {{hours: string, seconds: string, minutes: string}|{hours: string, seconds: string, minutes: string, days: string}|{hours: string, seconds: string, minutes: string, day: string}|{hours: string, seconds: string, minutes: string}}
  */
 export function formatCountdown(timestamp = 0, format = 'hh:mm:ss') {
   const hasDay = format === 'dd:hh:mm:ss';
@@ -41,5 +42,23 @@ export function formatCountdown(timestamp = 0, format = 'hh:mm:ss') {
     hours: `${`${hours}`.padStart(2, '0')}`,
     minutes: `${`${minutes}`.padStart(2, '0')}`,
     seconds: `${`${seconds}`.padStart(2, '0')}`
+  };
+}
+
+/**
+ * @description format timestamp
+ * @param timestamp
+ * @returns {{date: number, hours: number, seconds: number, month: number, year: number, minutes: number}}
+ */
+export function formatTimestamp(timestamp) {
+  const date = timestamp ? new Date(timestamp) : new Date();
+
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    date: date.getDate(),
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
   };
 }
